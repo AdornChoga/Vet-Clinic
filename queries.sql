0a8e130e5669ac851848b36739a07beb7f20b0d1
AdornChoga <adornchoga@gmail.com>
2022-03-22 17:09:37 +0200

    Set species to pokemon for all animals that don't have species already set.

--- queries.sql
+++ queries.sql
@@ -34,8 +34,8 @@ UPDATE animals
 SET species = 'digimon'
 WHERE name LIKE '%mon';
 UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon';
+SET species = 'pokemon'
+WHERE species IS NULL;
 COMMIT;
 
 BEGIN;
